f355c92ee9d87be128685e69f6b9075bfcf15c6e
Madhusudhan K <madhumk0709m@gmail.com>
2024-04-01 00:35:34 +0530

    added form validation project using javascript

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,27 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+$(document).ready(function(){
+    $(".main_content").waypoint(function(){
+        $(".main_content").addClass("animate__animated animate__fadeInLeft");
+    },{offset:"50%"});
+});
+
+$(document).ready(function(){
+    $(".main1_content").waypoint(function(){
+        $(".main1_content").addClass("animate__animated animate__fadeInLeft");
+    },{offset:"75%"});
+});
+
+
+$(document).ready(function(){
+    $(".main2_content").waypoint(function(){
+        $(".main2_content").addClass("animate__animated animate__fadeInDown");
+    },{offset:"75%"});
+});
+
+$(document).ready(function(){
+    $(".main3_content").waypoint(function(){
+        $(".main3_content").addClass("animate__animated animate__fadeInDown");
+    },{offset:"75%"});
+});
